3119a64680844fe24f1522bd5ccc70e91fb87cad
DryundeL <andromedich021@gmail.com>
2024-12-09 05:06:56 +0300

    Init commit

---
+++ cmd/music-lib/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// TODO: define config
+	// TODO: define logger
+	// TODO: define postgres
+	// TODO: define router
+	// TODO: run server
+	// TODO: Graceful shutdown
+}
